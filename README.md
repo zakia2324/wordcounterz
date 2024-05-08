ebe2d8363b4e23486e3052c4fbcc839493b3d940
zakia2324 <160940055+zakia2324@users.noreply.github.com>
2024-05-09 01:05:59 +0500

    enter a console

---
+++ README.md
@@ -0,0 +1,1 @@
+this is the word counter game,
---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgMagenta("\n \t\t welcome to word counter"));
+console.log(chalk.bgRed `#`.repeat(60));
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence:"
+    }
+]);
+let words = answers.sentence.trim().split(" ");
+console.log(chalk.bgRed `#`.repeat(70));
+console.log(" Sentence Words:");
+console.log(words);
+console.log(chalk.bold.bgBlueBright(`\n Word Count: ${words.length}`));
